cebb16bd152a351dc170da8bc4042765e94ea457
Aleksandar Kostadinov <alexander.kostadinov06@gmail.com>
2023-07-04 01:34:43 +0300

    Closed the navmenu on link click

--- script.js
+++ script.js
@@ -6,4 +6,9 @@ hamburger.addEventListener("click", () => {
     navbarUl.classList.toggle("active");
 });
 
-document.querySelectorAll(".nav-link");
+document.querySelectorAll(".nav-link").forEach((n) =>
+    n.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navbarUl.classList.remove("active");
+    })
+);
